6e464871c3afa975a690d684c546f26895859c5f
naaandato <naaandato@gmail.com>
2017-07-22 21:00:04 +0200

    initial import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service'
 import { User } from "./models/user";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Musify';
   user: User;
-  identity = true;
+  identity = false;
   token;
 
-  constructor(){
+  constructor(
+    private _userService:UserService
+  ){
     this.user = new User('','','','','','ROLE_USER','');
+  }
+
+  ngOnInit(){
+
+  }
+
+  public onSubmit() {
+    console.log(this.user);
   }
 }
