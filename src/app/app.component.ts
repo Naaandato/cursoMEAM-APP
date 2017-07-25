d276a87b6529796a414935f34ee538cc9f36a215
naaandato <naaandato@gmail.com>
2017-07-25 22:02:51 +0200

    curso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { User } from "./models/user";
 export class AppComponent implements OnInit{
   public title = 'Musify';
   public user: User;
-  public identity = false;
+  public identity;
   public token;
   public errorMessage;
 
@@ -23,6 +23,11 @@ export class AppComponent implements OnInit{
   }
 
   ngOnInit(){
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+
+    console.log(this.identity);
+    console.log(this.token);
 
   }
 
@@ -31,7 +36,57 @@ export class AppComponent implements OnInit{
 
     this._userService.signup(this.user).subscribe(
       response => {
+
+        this.identity = response.user;
+
+        if(!this.identity._id){
+          alert("el usuario no esta correctamente identificado");
+        }else{
+          //crear elemento en el localstorage para tener al usuario en sesion
+          localStorage.setItem('identity', JSON.stringify(this.identity));
+
+          //conseguir el token para enviarlo en cada peticion http
+          this._userService.signup(this.user, 'true').subscribe(
+            response => {
+
+              this.token = response.token;
+
+              if(this.token.length <= 0){
+                alert("el token no se ha generado correctamente");
+              }else{
+                //crear elemento en el localstorage para tener el token disponible
+                localStorage.setItem('token', this.token);
+
+                console.log(this.token)
+                console.log(this.identity);
+
+              }
+
+              console.log(response);
+
+            },
+            error => {
+              var errorMessage = <any>error;
+
+              if(errorMessage != null){
+                var body = JSON.parse(error._body);
+                this.errorMessage = body.message;
+
+              }
+            }
+          );
+
+
+
+
+
+
+
+
+        }
+
         console.log(response);
+
       },
       error => {
         var errorMessage = <any>error;
