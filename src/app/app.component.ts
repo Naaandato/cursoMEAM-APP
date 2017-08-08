2beb913ff8a594fc68d08fe9d7b4126e768a39f9
hivetry <hivetry@gmail.com>
2017-08-08 13:18:07 +0200

    fin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './services/user.service'
-import { User } from "./models/user";
+import { User } from './models/user';
+import { GLOBAL } from './services/global';
 
 
 @Component({
@@ -16,17 +17,21 @@ export class AppComponent implements OnInit{
   public identity;
   public token;
   public errorMessage;
+  public alertRegister;
+  public url;
 
   constructor(
     private _userService:UserService
   ){
     this.user = new User('','','','','','ROLE_USER','');
     this.userRegister = new User('','','','','','ROLE_USER','');
+
   }
 
   ngOnInit(){
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
+    this.url = GLOBAL.url;
 
     console.log(this.identity);
     console.log(this.token);
@@ -58,9 +63,7 @@ export class AppComponent implements OnInit{
               }else{
                 //crear elemento en el localstorage para tener el token disponible
                 localStorage.setItem('token', this.token);
-
-                console.log(this.token)
-                console.log(this.identity);
+                this.user = new User('','','','','','ROLE_USER','');
 
               }
 
@@ -111,7 +114,38 @@ export class AppComponent implements OnInit{
     this.token = null;
   }
 
+
+
   onSubmitRegister(){
     console.log(this.userRegister);
+
+    this._userService.register(this.userRegister).subscribe(
+
+      response => {
+        let user = response.user;
+        this.userRegister = user;
+
+        if (!user._id){
+          this.alertRegister = 'Error al registrarse';
+        }else{
+          this.alertRegister = 'El registro se ha realizado correctamente, identificate con ' + this.userRegister.email;
+          this.userRegister =  new User('','','','','','ROLE_USER','');
+        }
+
+      },
+      error => {
+        var errorMessage = <any>error;
+
+        if(errorMessage != null){
+          var body = JSON.parse(error._body);
+          this.alertRegister = body.message;
+
+        }
+      }
+
+
+
+
+    );
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
+import { routing, appRoutingProviders } from "./app.routing"
+
+import { AppComponent } from './app.component';
+import { UserEditComponent } from './components/userEdit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserEditComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [appRoutingProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
